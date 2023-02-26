26e0419cedd8fcca258f05baee6e906c8ecc27b2
ChenBr <chenbrilling1@gmail.com>
2023-02-26 14:35:16 +0200

    Replaced FontAwesome installation approach

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div class="text-5xl text-red-500 bg-slate-600 p-32 m-4">
-      test
-    </div>
+  <div class="bg-red-200">
+    <div class="text-5xl text-red-500 bg-slate-600 p-32 m-4">test</div>
+    <i class="fa-solid fa-user"></i>
+    <!-- uses solid style -->
+    <i class="fa-brands fa-github-square"></i>
+    <!-- uses brand style -->
+  </div>
 </template>
