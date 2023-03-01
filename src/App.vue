a873268cb413fd4f1f3105531313dd60988d64de
ChenBr <chenbrilling1@gmail.com>
2023-03-01 04:05:12 +0200

    Added about me base component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script>
 import NavBar from "./components/NavBar.vue";
+import AboutMe from "./components/AboutMe.vue";
 
 export default {
   components: {
     NavBar,
+    AboutMe,
   },
 };
 </script>
 
 <template>
   <NavBar />
+  <div class="content">
+    <AboutMe />
+  </div>
 </template>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div class="flex">
+      <div class="basis-3/5">
+        <h1>PsyChen</h1>
+        <h2>PsychoPy Experiment Creation</h2>
+        <p>
+          Hey, my name is Chen Brilling. I'm a B.A. student in Psychology in my
+          last semester at Bar-Ilan University. During the previous two years,
+          I've worked as a research assistant in the Social Neuroscience Lab,
+          where I developed a keen interest in creating experiments. As a
+          research assistant, I've gained a lot of hands-on experience with
+          various tools, including EEG, physiological systems, FFMPEG, OpenPose,
+          and experiment creation using PsychoPy, Labvanced, and Qualitrics.
+          Until recently, I have also been doing an internship as a Front-End
+          Developer in Labvanced, another excellent platform for online
+          experiments creation. I have experience with data processing tools,
+          including SPSS, Jamovi, and Excel (VBA). I have a broad understanding
+          of Javascript and Python and have created many experiments using
+          PsychoPy. I also have expertise in editing tools such as Photoshop,
+          After Effects, and Illustrator, which always come in handy. And many
+          more tools.
+        </p>
+      </div>
+      <div class="basis-2/5">image</div>
+    </div>
+    <button>Contant Me</button>
+  </section>
+</template>
