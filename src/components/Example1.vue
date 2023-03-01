881ded5466bad1cf1cba935619ba7d2756c5c472
ChenBr <chenbrilling1@gmail.com>
2023-03-01 20:38:40 +0200

    Added spreadsheet using airtable

--- src/components/Example1.vue
+++ src/components/Example1.vue
@@ -1,8 +1,14 @@
 <template>
   <section id="example_1">
     <div class="content">
-      <div class="flex gap-12">
-        <video class="basis-1/2 max-w-[50%] rounded-md" autoplay muted controls>
+      <div class="flex flex-col-reverse lg:flex-row gap-12">
+        <video
+          class="basis-1/2 lg:max-w-[50%] rounded-md"
+          autoplay
+          muted
+          controls
+          loop
+        >
           <source src="./../assets/videos/Vid1_720p.mp4" type="video/mp4" />
         </video>
         <div class="basis-1/2">
@@ -20,18 +26,29 @@
             uncertain. The pilot phase starts soon, and the actual run takes
             place within a few months.
           </p>
-          <div class="flex flex-row gap-4 lg:gap-12 mt-6">
+          <div
+            class="flex flex-row justify-center lg:justify-start gap-4 lg:gap-8 mt-6"
+          >
             <button class="flex items-center gap-2 p-2">
               <img class="w-8 h-8" src="./../assets/images/github_button.png" />
-              <p class="">Github Repository</p>
+              <p class="px-1">Github Repository</p>
             </button>
-            <button class="flex items-center gap-2 p-2">
+            <button class="flex items-center gap-2 px-2">
               <img class="w-8 h-8" src="./../assets/images/flow_button.png" />
-              <p class="">Flow</p>
+              <p class="px-1">Flow</p>
             </button>
           </div>
         </div>
       </div>
+      <iframe
+        class="airtable-embed mt-12 lg:mt-24 rounded-lg"
+        src="https://airtable.com/embed/shrd3r8uU3J7O5zFX?backgroundColor=cyan"
+        frameborder="0"
+        onmousewheel=""
+        width="100%"
+        height="533"
+        style="background: transparent; border: 1px solid #ccc"
+      ></iframe>
     </div>
   </section>
 </template>
