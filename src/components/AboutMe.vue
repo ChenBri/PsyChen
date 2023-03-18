dd8d47dfd02c6865ae39e318d2d244ecc464e0a1
ChenBr <chenbrilling1@gmail.com>
2023-03-19 00:35:45 +0200

    Updated paragraphs and buttons

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -20,23 +20,32 @@ export default {
         <div class="flex flex-col gap-4 basis-3/5">
           <h1>PsyChen</h1>
           <h2>PsychoPy Experiment Creation</h2>
-          <p class="leading-12">
-            Hey, my name is Chen Brilling. I'm a B.A. student in Psychology in
-            my last semester at Bar-Ilan University. During the previous two
-            years, I've worked as a research assistant in the Social
-            Neuroscience Lab, where I developed a keen interest in creating
-            experiments. As a research assistant, I've gained a lot of hands-on
-            experience with various tools, including EEG, physiological systems,
-            FFMPEG, OpenPose, and experiment creation using PsychoPy, Labvanced,
-            and Qualitrics. Until recently, I have also been doing an internship
-            as a Front-End Developer in Labvanced, another excellent platform
-            for online experiments creation. I have experience with data
-            processing tools, including SPSS, Jamovi, and Excel (VBA). I have a
-            broad understanding of Javascript and Python and have created many
-            experiments using PsychoPy. I also have expertise in editing tools
-            such as Photoshop, After Effects, and Illustrator, which always come
-            in handy. And many more tools.
-          </p>
+          <div>
+            <p class="leading-12">
+              Hey, my name is Chen Brilling. I'm a B.A. student in Psychology at
+              Bar-Ilan University. During the previous two years, I've worked as
+              a research assistant in the Social Neuroscience Lab, where I
+              developed a keen interest in experiment creation. As a research
+              assistant, I've gained much hands-on experience with various
+              tools, including PsychoPy, Labvanced, and Qualitrics. Until
+              recently, I have also been interning as a Front-End Developer in
+              Labvanced, another platform for creating online experiments.
+            </p>
+            <p class="leading-12 mt-2">
+              I have experience with data processing tools, including SPSS,
+              Jamovi, and Excel (VBA). I also have expertise in design and
+              animation tools such as Photoshop, After Effects, and Illustrator,
+              which always come in handy.
+            </p>
+          </div>
+          <div class="hidden lg:flex justify-center lg:justify-start">
+            <button
+              @click="scrollToId(`contact_me`)"
+              class="px-10 py-4 mt-12 lg:mt-6 bg-psy-blue rounded-lg text-white font-quicksand"
+            >
+              Contant Me
+            </button>
+          </div>
         </div>
         <div class="flex basis-2/5 justify-end mt-12">
           <div class="relative w-[80%]">
@@ -63,7 +72,7 @@ export default {
           </div>
         </div>
       </div>
-      <div class="flex justify-center lg:justify-start">
+      <div class="flex lg:hidden jlex justify-center lg:justify-start">
         <button
           @click="scrollToId(`contact_me`)"
           class="px-10 py-4 mt-12 lg:mt-6 bg-psy-blue rounded-lg text-white font-quicksand"
