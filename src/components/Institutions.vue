316fbf4622ff8d2aba86db1c9897a21f2f1393fc
ChenBr <chenbrilling1@gmail.com>
2023-05-30 23:43:50 +0300

    Added institutions responsive design

--- src/components/Institutions.vue
+++ src/components/Institutions.vue
@@ -6,26 +6,32 @@ export default {
         {
           name: "Bar-Ilan University",
           path: "dynamic-images/inst-biu.png",
+          link: "",
         },
         {
           name: "Doha Institute for Graduate Studies",
           path: "dynamic-images/inst-doha.png",
+          link: "",
         },
         {
           name: "University of Nottingham",
           path: "dynamic-images/inst-uon.png",
+          link: "",
         },
         {
           name: "University of Cambridge",
           path: "dynamic-images/inst-camb.png",
+          link: "",
         },
         {
           name: "Reichman University",
           path: "dynamic-images/inst-ru.png",
+          link: "",
         },
         {
           name: "George Mason University",
           path: "dynamic-images/inst-ma.png",
+          link: "",
         },
       ],
     };
@@ -34,13 +40,28 @@ export default {
 </script>
 
 <template>
-  <section class="bg-psy-bg-dark relative pt-6 pb-4">
+  <section class="bg-psy-bg-dark relative pt-6 pb-2">
     <div class="content">
-      <h2 class="text-white text-2xl">Institutions I have been working with</h2>
-      <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 mx-12">
+      <h2 class="text-white text-2xl text-center mb-[0.75em] font-normal">
+        Institutions I have been working with:
+      </h2>
+      <div
+        class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 mx-12 mb-6 bg-[#0f1d4a] rounded-xl border-2 border-white border-opacity-70 p-2"
+      >
         <div v-for="image in imagesList">
-          <img :src="image.path" :alt="image.name" class="px-8 pt-8 pb-4" />
-          <div class="text-center text-white font-roboto">{{ image.name }}</div>
+          <img
+            :src="image.path"
+            :alt="image.name"
+            :title="image.name"
+            class="p-2 w-28 h-28 mx-auto"
+          />
+          <div class="flex items-center justify-center">
+            <div
+              class="text-center text-white font-roboto w-4/5 text-sm md:text-base"
+            >
+              {{ image.name }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
